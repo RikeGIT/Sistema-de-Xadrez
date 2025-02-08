3b44cd85faceeb8193fd51706042ca505a0afdea
rike-git <rikecontatopessoal@gmail.com>
2025-02-08 16:16:01 -0300

    Implementando os movimentos da Torre

--- src/chess/ChessMatch.java
+++ src/chess/ChessMatch.java
@@ -28,6 +28,7 @@ public class ChessMatch {
         Position source = sourcePosition.toPosition();
         Position target = targetPosition.toPosition();
         validateSourcePosition(source);
+        validateTargetPosition(source, target);
         Piece capturedPiece = makeMove(source, target);
         return (ChessPiece)capturedPiece;
     }
@@ -36,6 +37,11 @@ public class ChessMatch {
         Piece capturedPiece = board.removePiece(target);
         board.placePiece(p, target);
         return capturedPiece;
+    }
+    private void validateTargetPosition(Position source, Position target){
+        if (!board.piece(source).possibleMove(target)){
+            throw new ChessException("A peça escolhida não pode se mover para a posição de destino");
+        }
     }
     private void validateSourcePosition(Position position){
         if (!board.thereIsAPiece(position)){
