3903a75d1156e66664a9cb656421d0613f8b17e7
rike-git <rikecontatopessoal@gmail.com>
2025-02-10 12:16:28 -0300

    Logica de check

--- src/application/UI.java
+++ src/application/UI.java
@@ -68,6 +68,10 @@ public class UI {
         System.out.println("Turno: " + chessMatch.getTurn());
         System.out.println("Esperando o jogador: " + chessMatch.getCurrentPlayer());
 
+        if (chessMatch.getCheck()){
+            System.out.println("Check");
+        }
+
     }
 
     public static void printBoard(ChessPiece[][] pieces, boolean[][] possibleMoves){
