920467e8222590dfcbe19780438223df84a5887e
rike-git <rikecontatopessoal@gmail.com>
2025-02-12 14:07:21 -0300

    inserindo a rainha e corrigindo o peão

--- src/chess/pieces/Pawn.java
+++ src/chess/pieces/Pawn.java
@@ -45,8 +45,6 @@ public class Pawn extends ChessPiece {
                 mat[p.getRow()][p.getColumn()] = true;
             }
 
-
-
         } else {
 
             // LÓGICA DO PEÃO PRETO
@@ -60,7 +58,7 @@ public class Pawn extends ChessPiece {
 
             // duas casas para frente
             p.setValues(position.getRow() + 2, position.getColumn());
-            Position p2 = new Position(position.getRow() - 1, position.getColumn());
+            Position p2 = new Position(position.getRow() + 1, position.getColumn());
             if (getBoard().positionExists(p) && !getBoard().thereIsAPiece(p) && getBoard().positionExists(p2) && !getBoard().thereIsAPiece(p2) && getMoveCount() == 0){
                 mat[p.getRow()][p.getColumn()] = true;
             }
